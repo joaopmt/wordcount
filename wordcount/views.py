4b120531937a7802fd5c97e1ca53ab9f44b7a7bc
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2019-02-07 05:01:41 -0200

    add homepage's form and count page

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello")
+    return render(request, 'homepage.html')
 
 
-def eggs(request):
-    return HttpResponse("Eggsss")
+def count(request):
+    return render(request, 'count.html')

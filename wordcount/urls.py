386c66ca55d1ef979baececb2d0f260c524df84a
joaopmt <joaop.congiomartins@gmail.com>
2019-02-09 14:04:51 -0200

    add about page

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
     path('count/', views.count, name='count'),
+    path('about/', views.about, name='about'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -12,3 +12,7 @@ def count(request):
     occ_dict = functions.get_occurrence_dict(fulltext.split())
     sorted_tuple = sorted(occ_dict.items(), key=operator.itemgetter(1), reverse=True)
     return render(request, 'count.html', {'fulltext': fulltext, 'sorted_tuple': sorted_tuple})
+
+
+def about(request):
+    return render(request, 'about.html')
